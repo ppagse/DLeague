5dd2d7a2046db7d5d73c10477b5a51218423f89b
ppagse <ppagse6@gmail.com>
2024-09-23 20:30:44 +0900

    commit

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import sqlite3
 conn = sqlite3.connect('football.db')
 cursor = conn.cursor()
 
+teams = ['김민규', '김환희', '노우찬', '박세중', '변상훈', '심이루', '염도현']
+
 def add_match(match_id, home_team, away_team):
     cursor.execute('''
         INSERT INTO matches (match_id, home_team, away_team)
@@ -52,26 +54,41 @@ def update_result(match_id, home_team, away_team, home_score, away_score):
         ''', (home_score - away_score, home_team)) 
     conn.commit()
 
+def reset():
+    cursor.execute('''
+    TRUNCATE TABLE matches
+    ''')
+    for team in teams:
+        cursor.execute('''
+            INSERT INTO standings (team, game, win, draw, lose, gd, point)
+            VALUES (?, 0, 0, 0, 0, 0, 0)
+        ''', (team,))
+    conn.commit()
 
 st.set_page_config(page_title='D리그 순위표')
 
 pages = ['일정', '순위표', '플레이오프', '결과 입력']
 page = st.sidebar.selectbox('', pages)
-teams = ['김민규', '김환희', '노우찬', '박세중', '변상훈', '심이루', '염도현']
-schedule = {}
+schedule = []
 
 if page == '일정':
     st.title('일정')
     widget_id = (id for id in range(1, 100_00))
-    for i in range(1, 15):
-        st.subheader(f'{i}라운드')
-        col1, col2 , col3= st.columns([2, 1, 2])
-        with col1:
-            home_team = st.selectbox("홈팀", teams, key = next(widget_id))
-        with col2:
-            st.write('vs')
-        with col3:
-            away_team = st.selectbox("원정팀", teams, key = next(widget_id))
+    for i in range(14):
+        st.subheader(f'{i+1}라운드')
+        for j in range(1, 4):
+            col1, col2 , col3= st.columns([4, 1, 4])
+            with col1:
+                home_team = st.selectbox("홈팀", teams, key = next(widget_id))
+            with col2:
+                st.write('vs')
+            with col3:
+                away_team = st.selectbox("원정팀", teams, key = next(widget_id))
+            schedule[i*3+j] = [home_team, away_team]
+    if st.button('리그 시작'):
+        reset()
+        for i in range(1, 43):
+            add_match(i, schedule[i][0], schedule[i][1])
 
 elif page == '순위표':
     pass
